3a958b55f606ea2e19c01123da38340cb18e6ccd
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-11-09 04:06:44 -0800

    added files

--- app.py
+++ app.py
@@ -22,3 +22,23 @@ def index():
     cur.close()
     conn.close()
     return render_template('index.html', books=books)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        pages_num = int(request.form['pages_pages_num'])
+        review = request.form['review']
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                    'VALUES (%s, %s, %s, %s',
+                    (title, author, pages_num, review))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+
+    return render_template('create.html')
