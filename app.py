b4ef9dc45dcb74f3d7b7b65b862d00d7bb2ae7a8
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-11-09 18:07:38 -0800

    added source

--- app.py
+++ app.py
@@ -29,13 +29,13 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         author = request.form['author']
-        pages_num = int(request.form['pages_pages_num'])
+        pages_num = int(request.form['pages_num'])
         review = request.form['review']
 
         conn = get_db_connection()
         cur = conn.cursor()
         cur.execute('INSERT INTO books (title, author, pages_num, review)'
-                    'VALUES (%s, %s, %s, %s',
+                    'VALUES (%s, %s, %s, %s)',
                     (title, author, pages_num, review))
         conn.commit()
         cur.close()
